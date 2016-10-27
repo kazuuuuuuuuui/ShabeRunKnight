3b5b6f8e92ec33dd7e7a905d184ba823bb23e537
kazuuuuuuuuui <kazuuuuuuuuui1105@yahoo.co.jp>
2016-10-27 20:13:25 +0900

    y座標を変えずにキャラクターを追従するカメラを実装しようとしたけどよくわからなかった

---
+++ Assets/Script/MainCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Camera camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //追従するキャラクターのy座標
+        float player_x = player.transform.position.x;
+
+        //現在のカメラのx
+        float camera_x = camera.transform.position.x;
+
+        //次のx
+        float new_x = Mathf.Lerp(camera_x, player_x, 0.5f);
+
+        transform.position = new Vector3(new_x, transform.position.y, transform.position.z);
+    }
+}
